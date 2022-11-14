27d1eb172ec3e4340125e425e80605922023036b
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-11-14 14:14:13 -0800

    Resource 5: POST '/' test written and passing

--- __tests__/cool-stores.test.js
+++ __tests__/cool-stores.test.js
@@ -10,6 +10,7 @@ describe('cool-stores routes', () => {
   it('GET /stores/1 should return a detailed object of store 1', async () => {
     const resp = await request(app).get('/stores/1');
     expect(resp.body).toEqual({
+      company: 'Kuvalis Group',
       department: 'Clothing',
       id: '1',
       item: 'shoe lace',
@@ -22,30 +23,35 @@ describe('cool-stores routes', () => {
     expect(resp.body).toMatchInlineSnapshot(`
       Array [
         Object {
+          "company": "Kuvalis Group",
           "department": "Clothing",
           "id": "1",
           "item": "shoe lace",
           "price": "$4.60",
         },
         Object {
+          "company": "Feeney, Abbott and Bednar",
           "department": "Home",
           "id": "2",
           "item": "throw pillow",
           "price": "$19.46",
         },
         Object {
+          "company": "Will, Mohr and Konopelski",
           "department": "Jewelry",
           "id": "3",
           "item": "nose ring",
           "price": "$2.34",
         },
         Object {
+          "company": "Lueilwitz Group",
           "department": "Electronics",
           "id": "4",
           "item": "VHS camera",
           "price": "$14.19",
         },
         Object {
+          "company": "Upton and Sons",
           "department": "Music",
           "id": "5",
           "item": "Beach Boys",
@@ -53,6 +59,19 @@ describe('cool-stores routes', () => {
         },
       ]
     `);
+  });
+  it('POST /stores should create a new store', async () => {
+    const newStore = {
+      company: 'Gorczany-Stanton',
+      department: 'Baby',
+      item: 'diapers',
+      price: '$12.51',
+    };
+    const resp = await request(app).post('/stores').send(newStore);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      ...newStore,
+    });
   });
   afterAll(() => {
     pool.end();
